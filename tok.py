5c9f5092368b1ff0a1e9b3ffceeeeff2d1b99e2d
drupchen <drupchen@dharmasagar.com>
2023-07-20 10:16:33 +0545

    improve support for news articles

--- tok.py
+++ tok.py
@@ -1,11 +1,28 @@
 from pathlib import Path
 
-from third_party.Botok.botok import Text
+from third_party.Botok.botok import Text, sentence_tokenizer, WordTokenizer
+
 
 infile = Path("input/articletest.txt")
 raw = infile.read_text(encoding="utf-8")
 
-t = Text(raw).tokenize_sentences_plaintext
+
+def sent_tok(raw):
+    w = WordTokenizer()
+    tokens = w.tokenize(raw, spaces_as_punct=True)
+    return sentence_tokenizer(tokens)
+
+
+def plaintext_sent_par(units, sep="\n") -> str:
+    out = []
+    for u in units:
+        unit = "".join([word.text for word in u['tokens']]).strip()
+        out.append(unit)
+    return sep.join(out)
+
+
+t = Text(raw).custom_pipeline("basic_cleanup", sent_tok, "dummy", plaintext_sent_par)
+
 
 outfile = Path("output") / infile.name
 
